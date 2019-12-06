d4b5c82ce8e52406b339550f6318f54c1370bc50
amirrezasadeqi <e.a.sadeqi@gmail.com>
2019-12-06 22:28:42 +0330

    compeleted the path planner node for a fixed landing target. the node publishes the quadrotor velocity command in the world frame.

--- src/cmd_vel_converter/src/quad_ori_tf_br.cpp
+++ src/cmd_vel_converter/src/quad_ori_tf_br.cpp
@@ -11,20 +11,25 @@ int main(int argc, char** argv){
     tf::TransformBroadcaster br;
     tf::Transform quad_ori_fram;
     tf::Transform quad_xy_frame;
+    tf::Transform turtlebot_ori_frame;
 
-    tf::TransformListener listener;
+    tf::TransformListener listener, turtle_listener;
 
-    ros::Rate rate(10);
+    ros::Rate rate(20);
 
     while (node.ok())
     {
-        tf::StampedTransform transform;
+        tf::StampedTransform transform, vel_transform;
         try
         {
             ros::Time current = ros::Time::now();
             listener.waitForTransform("world", "base_link", current, ros::Duration(3.0));
             // this is transform between quadrotor and world frame."quad wrt world"
             listener.lookupTransform("world", "base_link", current, transform);
+
+            turtle_listener.waitForTransform("world", "ugv/base_link", current, ros::Duration(3.0));
+            turtle_listener.lookupTransform("world", "ugv/base_link", current, vel_transform);
+
         }
         catch(tf::TransformException ex)
         {
@@ -38,14 +43,17 @@ int main(int argc, char** argv){
         quad_xy_frame.setOrigin(tf::Vector3(transform.getOrigin().x(), transform.getOrigin().y(), 0));
         quad_xy_frame.setRotation(tf::Quaternion(0, 0, 0, 1));
 
+        // turtlebot orientation frame
+        turtlebot_ori_frame.setOrigin(rf::Vector3(0, 0, 0));
+        turtlebot_ori_frame.setRotation(vel_transform.getRotation());
+        
 
         br.sendTransform(tf::StampedTransform(quad_ori_fram, ros::Time::now(), "world", "quad_orientation"));
         br.sendTransform(tf::StampedTransform(quad_xy_frame, ros::Time::now(), "world", "quad_translation"));
-        
+        br.sendTransform(tf::StampedTransform(turtlebot_ori_frame, ros::Time::now(), "world", "turtlebot_orientation"));
+
         rate.sleep();
     }
-    
-
 
     return 0;
 }
--- src/path_planner/src/wr_quad_vel_pub.cpp
+++ src/path_planner/src/wr_quad_vel_pub.cpp
@@ -1,14 +1,22 @@
 #include<ros/ros.h>
 #include<tf/transform_listener.h>
 #include<nav_msgs/Odometry.h>
+#include<geometry_msgs/Vector3.h>
+#include<math.h>
+
+
+// parameters of landing algorithm
+float kdg, desired_time;
 
 // writing classes to listen to states of the quadrotor
 // and the turtlebot.
 
 class quad_state_listener {
     public:
-        // variable to save the state of the quadrotor
-        nav_msgs::Odometry quadrotor_state;
+        // variable to save the state of the quadrotor. we can use the 
+        // quadrotor_initial_state for determining the d_0.
+        nav_msgs::Odometry quadrotor_state, quadrotor_initial_state;
+        int count = 0;
 
         // callback function on quad state receiving
         void quad_state_callback(const nav_msgs::Odometry::ConstPtr& state){
@@ -18,6 +26,11 @@ class quad_state_listener {
             quadrotor_state.pose = state->pose;
             quadrotor_state.twist = state->twist;
 
+            if(count == 0){
+                quadrotor_initial_state = quadrotor_state;
+            }
+
+            count = count + 1;
         }
 
 };
@@ -25,7 +38,10 @@ class quad_state_listener {
 class turtlebot_state_listener {
     public:
         // variable to save the turtlebot state
-        nav_msgs::Odometry turtlebot_state;
+        // we can use the turtlebot_initial_state for determine d_0
+        // without transforming to other frames.
+        nav_msgs::Odometry turtlebot_state, turtlebot_initial_state;
+        int count = 0;
     
         // callback function to save the state of the turtlebot
         void turtlebot_state_callback(const nav_msgs::Odometry::ConstPtr& state){
@@ -34,6 +50,12 @@ class turtlebot_state_listener {
             turtlebot_state.child_frame_id = state->child_frame_id;
             turtlebot_state.pose = state->pose;
             turtlebot_state.twist = state->twist;
+
+            if(count == 0){
+                turtlebot_initial_state = turtlebot_state;
+            }
+
+            count = count + 1;
         }
 
 };
@@ -44,6 +66,13 @@ int main(int argc, char** argv){
 
     ros::NodeHandle node;
 
+    // getting the command line inputs
+    kdg = atof(argv[1]);
+    desired_time = atof(argv[2]);
+    double d_0;
+
+    ros::Time td(desired_time);
+
     // reading the state of quadrotor
     quad_state_listener quad_state_listener;
     ros::Subscriber quad_state_sub = node.subscribe("/drone/ground_truth/state", 20, &quad_state_listener::quad_state_callback, &quad_state_listener);
@@ -52,7 +81,7 @@ int main(int argc, char** argv){
     turtlebot_state_listener turtlebot_state_listener;
     ros::Subscriber turtlebot_state_sub = node.subscribe("/ugv/odom", 20, &turtlebot_state_listener::turtlebot_state_callback, &turtlebot_state_listener);
 
-    // now let's convert the turtlebot velocity to the world frame
+    // now let's convert the turtlebot linear velocity to the world frame
     // to do this we must listen to the tf topic for ugv/base_link
     // to the world. but note that we don't have translation for 
     // velocity:
@@ -64,11 +93,33 @@ int main(int argc, char** argv){
     // a transform derived from ugv wrt world for transforming 
     // velocity without translation.
     tf::Transform ugv_world_vel_transform;
+    
+    ros::Time start_time = ros::Time::now();
+
+    // variable to save the quadrotor velocity command to send to ros 
+    // network
+    geometry_msgs::Vector3 cmd_vel;
+
+    // publisher to publish the cmd_vel
+    ros::Publisher cmd_vel_pub = node.advertise<geometry_msgs::Vector3>("cmd_vel_wr", 10);
+
+    ros::Rate rate(20);
 
     while (node.ok())
     {
-
+        
         ros::Time current = ros::Time::now();
+        
+        double time = current.toSec() - start_time.toSec();
+
+        // an stamped point to save the turtlebot velocity in base_link and world frames
+        tf::Stamped<tf::Vector3> turtle_vel_in, turtle_vel_out;
+        turtle_vel_in.frame_id_ = "turtlebot_orientation";
+        turtle_vel_in.stamp_ = current;
+        turtle_vel_in.x = turtlebot_state_listener.turtlebot_state.twist.twist.linear.x;
+        turtle_vel_in.y = turtlebot_state_listener.turtlebot_state.twist.twist.linear.y;
+        turtle_vel_in.z = turtlebot_state_listener.turtlebot_state.twist.twist.linear.z;
+
 
         // subscribing to the ugv wrt world transform
         try
@@ -82,13 +133,40 @@ int main(int argc, char** argv){
             ros::Duration(1.0).sleep();
         }
         // now ugv to world without translation
-        ugv_world_vel_transform.
-        
+        ugv_world_vel_transform.setOrigin(tf::Vector3(0, 0, 0));
+        ugv_world_vel_transform.setRotation(ugv_world_transform.getRotation());
         
 
+        tf::Transformer transformer;
+        // we use turtle_vel_out for vm in the path plannin algorithm
+        transformer.transformPoint("world", turtle_vel_in, turtle_vel_out);
+
+        // let's determine the d_0
+        if (turtlebot_state_listener.count == 1){
+            d_0 = sqrt(pow((quad_state_listener.quadrotor_initial_state.pose.pose.position.x - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.x), 2) + pow((quad_state_listener.quadrotor_initial_state.pose.pose.position.y - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.y), 2) + pow((quad_state_listener.quadrotor_initial_state.pose.pose.position.z - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.z), 2));
+        }
+
+        // determine d_dot
+        double d_dot = (((-2 * d_0 * time) / (kdg * pow(td.toSec(), (2 / kdg)))) * pow((pow(td.toSec(), 2) - pow(time, 2)), ((1 / kdg) - 1)));
+
+        // now determine the quadrotor velocity command and publish it to the ros network
+        // note that we use for now the landing target stoped so use the initial state of
+        // the turtlebot for x_td.
+        // TODO: change the code for moving landing target.
+
+        cmd_vel.x = turtle_vel_out.x + (d_dot / d_0) * (quad_state_listener.quadrotor_initial_state.pose.pose.position.x - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.x);
+        cmd_vel.y = turtle_vel_out.y + (d_dot / d_0) * (quad_state_listener.quadrotor_initial_state.pose.pose.position.y - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.y);
+        cmd_vel.z = (d_dot / d_0) * (quad_state_listener.quadrotor_initial_state.pose.pose.position.z - turtlebot_state_listener.turtlebot_initial_state.pose.pose.position.z);
+
+        // now publish the quadrotor velocity command with respaect to the world
+        cmd_vel_pub.publish(cmd_vel);
+
+        
         // this is for checking the topics and subscribing
         // to them in each loop.
-        ros::spinOnce();        
+        ros::spinOnce();    
+
+        rate.sleep();    
     }
     
 
